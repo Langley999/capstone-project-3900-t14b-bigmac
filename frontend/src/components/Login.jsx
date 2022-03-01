1baf1da99989fc9eeac2be28ccdf902568d05ceb
z5310223 <z5310223@ad.unsw.edu.au>
2022-03-01 14:19:49 +1100

    added input boxes for login, need to format

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,17 +1,104 @@
-
-import axios from "axios";
 import React, { useState, useRef } from 'react';
-
-
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormControl from '@mui/material/FormControl';
+import TextField from '@mui/material/TextField';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
 
 
 const Login = () => {
+  const [values, setValues] = React.useState({
+    password: '',
+    showPassword: false,
+  });
 
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
 
-    return (  
-      <h1>login</h1>
-      
+  const handleClickShowPassword = () => {
+    setValues({
+      ...values,
+      showPassword: !values.showPassword,
+    });
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+    return (
+      <div>
+        <h1>login</h1>
+        <Box
+          component="form"
+          noValidate
+          autoComplete="off"
+        >
+          <TextField
+            required
+            id="outlined-username"
+            label="Username"
+            sx={{ m: 1, width: '25ch' }}
+          />
+          <TextField
+            required
+            id="outlined-email"
+            label="Email"
+            type="email"
+            sx={{ m: 1, width: '25ch' }}
+          />
+          <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+            <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+            <OutlinedInput
+              id="outlined-adornment-password"
+              type={values.showPassword ? 'text' : 'password'}
+              value={values.password}
+              onChange={handleChange('password')}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    edge="end"
+                  >
+                    {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              }
+              label="Password"
+            />
+          </FormControl>
+          <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+            <InputLabel htmlFor="outlined-adornment-password">Confirm Password</InputLabel>
+            <OutlinedInput
+              id="outlined-adornment-password2"
+              type={values.showPassword ? 'text' : 'password'}
+              value={values.password}
+              onChange={handleChange('password')}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    edge="end"
+                  >
+                    {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              }
+              label="Confirm Password"
+            />
+          </FormControl>
+        </Box>
+      </div>
     );
   };
 export default Login;
