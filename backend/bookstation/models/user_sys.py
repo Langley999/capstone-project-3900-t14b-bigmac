9caf962fb9c9fdfc1efd12eaf823b3a6a8d7aec7
Yasin Khan <z5265047@ad.unsw.edu.au>
2022-03-09 21:29:18 +1100

    fixed relationships, added implemenation for book details, reviews

--- backend/bookstation/models/user_sys.py
+++ backend/bookstation/models/user_sys.py
@@ -1,10 +1,17 @@
 from bookstation import db
 from bookstation.models.book_sys import Collection_book, Review
-
+'''
 follow_relationship = db.Table('follow_relationship',
     db.Column('follower_user_id', db.Integer, db.ForeignKey('user.user_id'), primary_key=True),
     db.Column('user_id', db.Integer, db.ForeignKey('user.user_id'), primary_key=True)
 )
+'''
+
+class Follow_relationship(db.Model):
+    follower_user_id = db.Column('follower_user_id', db.Integer, db.ForeignKey('user.user_id'), primary_key=True)
+    user_id = db.Column('user_id', db.Integer, db.ForeignKey('user.user_id'), primary_key=True)
+    follower_user = db.relationship("User", foreign_keys=[follower_user_id])
+    followed_user = db.relationship("User", foreign_keys=[user_id])
 
 class User(db.Model):
 
@@ -14,14 +21,10 @@ class User(db.Model):
     username = db.Column(db.String(32), unique=True, nullable=False)
     email = db.Column(db.String(64), unique=True, nullable=False)
     password = db.Column(db.String(256))
-    posts = db.relationship('Post', backref='user', lazy=True)
-    collections = db.relationship('Collection', backref='user', lazy=True)
-    reviews = db.relationship('Book', secondary=Review.__tablename__, backref='user')
-    followers = db.relationship('User',
-                                secondary=follow_relationship,
-                                primaryjoin=user_id==follow_relationship.c.user_id,
-                                secondaryjoin=user_id==follow_relationship.c.follower_user_id,
-                                backref="followings")
+    #posts = db.relationship('Post')
+    #collections = db.relationship('Collection')
+    #reviews = db.relationship('Review')
+    #followers = db.relationship('Follow_relationship')
 
     def __init__(self, username, email, password):
         self.username   = username
@@ -36,6 +39,7 @@ class Post(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=False)
     content = db.Column(db.String(1024))
     created_time = db.Column(db.Time)
+    user = db.relationship('User')
 
     def __init__(self, content):
         self.content = content
@@ -49,7 +53,8 @@ class Collection(db.Model):
     is_default = db.Column(db.Integer)
     created_time = db.Column(db.Time)
     user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=False)
-    books = db.relationship('Book', secondary=Collection_book.__tablename__, backref='collection')
+    books = db.relationship('Collection_book')
+    user = db.relationship('User')
 
     def __init__(self, name):
         self.is_default = 0
