5911ba46242e7a05179fd9eda1f0defe2cb6b78a
Yasin Khan <z5265047@ad.unsw.edu.au>
2022-03-12 22:47:50 +1100

    updated init file for session config

--- backend/bookstation/__init__.py
+++ backend/bookstation/__init__.py
@@ -19,9 +19,13 @@ def defaultHandler(err):
     return response
 
 app = Flask(__name__)
-app.secret_key = 'BIGMAC'
+#app.secret_key = 'BIGMAC'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bookstation.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'BIGMAC'
+app.config['SESSION_TYPE'] = 'filesystem'
+
+
 CORS(app)
 Session(app)
 app.register_error_handler(Exception, defaultHandler)
