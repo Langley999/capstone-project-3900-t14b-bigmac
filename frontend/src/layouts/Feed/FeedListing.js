2477720f676d74f69f2182bbd2218d4e3a33c6ad
janeyin1107 <jane.jingyuan.yin@gmail.com>
2022-03-26 12:47:40 +1100

    fix date

--- frontend/src/layouts/Feed/FeedListing.js
+++ frontend/src/layouts/Feed/FeedListing.js
@@ -11,25 +11,26 @@ const FeedListing = ({ post, isPublic }) => {
     let li = str.split(' ');
     let time = li[1];
     const date = new Date(li[0].replace(/-/g,"/"));
-
     return date;
   }
-  const formatAMPM = (date) => {
-    let hours = date.getHours();
-    let minutes = date.getMinutes();
+  const formatAMPM = (str) => {
+    let li = str.split(' ');
+    let time = li[1].split(':');
+    let hours = time[0];
+    let minutes = time[1];
     let ampm = hours >= 12 ? 'PM' : 'AM';
     hours = hours % 12;
     hours = hours ? hours : 12;
-    minutes = minutes < 10 ? '0'+minutes : minutes;
     let strTime = hours + ':' + minutes + ampm;
     return strTime;
+
   }
   return (
     <Card>
       <CardContent>
         <div style={{display: "flex", flexDirection: "row", justifyContent: "space-between"}} >
           <UsernameLink username={post.username} id={post.user_id} avatar={post.avatar} isPublic={isPublic} />
-          <div>{formatAMPM(createDate(post.time_created))} {createDate(post.time_created).toLocaleDateString("en-AU")}</div>
+          <div>{formatAMPM(post.time_created)} {createDate(post.time_created).toLocaleDateString("en-AU")}</div>
         </div>
         {post.content}
       </CardContent>
