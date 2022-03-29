34d2397cd3de885ef4bb4e39e6e7e7235b932d89
z5310223 <z5310223@ad.unsw.edu.au>
2022-03-29 14:49:55 +1100

    error with submitting verification code

--- backend/bookstation/controllers/auth/auth.py
+++ backend/bookstation/controllers/auth/auth.py
@@ -139,6 +139,7 @@ def sendCode():
     try:
         msg = Message("Verification Code: ", sender= 'bigmaccomp3900@gmail.com', recipients=[email])
         code[email] = randint(10000, 99999)
+        print(code[email])
         msg.body = f"Your reset code is {str(code)}"
         mail.send(msg)
     except:
@@ -154,8 +155,13 @@ def sendCode():
 def verify():
     data = request.get_json()
     username, email, password, user_code = data['username'], data['email'], data['password'], data['user_code']
-    print('stored code is: ', code)
-    if code[email] != user_code:
+    print('before string conversion', email)
+    email = str(email)
+    print('after conversion', email)
+    print('stored code is: ', code[email])
+    print('frontend code', user_code, email)
+    if int(code[email]) != int(user_code):
+        print('comparison failed', code)
         raise error.InputError(description="Verification code does not match")
     code.pop(email, None)
     token = generate_token(username)
@@ -169,6 +175,7 @@ def verify():
     db.session.add(new_default_collection)
     db.session.add(new_history_collection)
     db.session.commit()
+    print(code)
     return dumps({
         'token': token,
         'user_id': new_user.user_id
