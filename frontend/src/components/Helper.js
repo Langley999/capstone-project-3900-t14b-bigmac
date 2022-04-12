89209f2cf07e400fa3e087995eaee0e7c37a5ab4
janeyin1107 <jane.jingyuan.yin@gmail.com>
2022-04-12 11:10:48 +1000

    sort

--- frontend/src/components/Helper.js
+++ frontend/src/components/Helper.js
@@ -3,8 +3,8 @@ export const url = 'http://localhost:8080';
 export const checkProfileInput = (username, email, password) => {
   const regex = '^[a-zA-Z0-9]+[\\._]?[a-zA-Z0-9]*[\\._]*[a-zA-Z0-9]+[@]\\w+[.].*\\w{2,3}$';
 
-  if (username.length > 50 || username.length < 1) {
-    return "Username has to be between 1 and 50 characters inclusively.";
+  if (username.length > 15 || username.length < 1) {
+    return "Username has to be between 1 and 15 characters inclusively.";
   } else if (password.length > 16 || password.length < 8) {
     return "Password has to be between 8 and 16 characters inclusively";
   } else if (email.length < 1) {
