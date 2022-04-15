39291b766f6a734dc1d26865b528ebec1487b4d9
Yasin Khan <z5265047@ad.unsw.edu.au>
2022-04-15 17:19:28 +1000

    added function and some inline comments

--- backend/bookstation/controllers/recommendation/recommendation.py
+++ backend/bookstation/controllers/recommendation/recommendation.py
@@ -43,10 +43,25 @@ def toprating():
 	})
 
 
+
+"""
+Description: 
+- Get recommendation based on user activity in particular the most most frequent Genre
+
+Args:
+- token String: to be used for session validation
+
+Return:
+- dict: Dictionary containing list of most popular Genres
+
+"""		
 @app.route(url_prefix + "/favouriteGenre", methods=["GET"])
 def favouriteGenre():
 	token = request.args.get('token')
 	user = get_user(token)
+
+	# get all books from all collections the current user has created
+	# add its book_id and genre to the set, set strucutre prevents duplication
 	book_set = set()
 	collections = Collection.query.filter_by(user_id = user.user_id).all()
 	for collection in collections:
@@ -56,6 +71,7 @@ def favouriteGenre():
 			if (book.book_id, book.genre_string) not in book_set:
 				book_set.add((book.book_id, book.genre_string))
 	
+	#create genre frequency mapping
 	genre_freq = {}
 	for (book_id, genres) in book_set:
 		genres_list = ast.literal_eval(genres)
@@ -69,11 +85,24 @@ def favouriteGenre():
 
 	return dumps({'favourite_genres' : max_genres})
 		
-		
+"""
+Description: 
+- Get recommendation based on user activity in particular the most most frequent Author's books
+
+Args:
+- token String: to be used for session validation
+
+Return:
+- dict: Dictionary containing list of Favourite Authors and their corresponding books
+
+"""		
 @app.route(url_prefix + "/favouriteAuthor", methods=["GET"])
 def favouriteAuthor():
 	token = request.args.get('token')
 	user = get_user(token)
+
+	# get all books from all collection the current user has created
+	# add its book_id and corresponding author to book set, set strucutre prevents duplication
 	book_set = set()
 	collections = Collection.query.filter_by(user_id = user.user_id).all()
 	for collection in collections:
@@ -83,8 +112,10 @@ def favouriteAuthor():
 			if (book.book_id, book.author_string) not in book_set:
 				book_set.add((book.book_id, book.author_string))
 	
+	# create frequency mapping for Authors
 	author_freq = {}
 	for (book_id, authors) in book_set:
+		# note: it is quicker to use pythons processing, than to query a m-m relationship table
 		authors_list = authors.split(", ")
 		for author in authors_list:
 			if author in author_freq:
@@ -92,10 +123,14 @@ def favouriteAuthor():
 			else:
 				author_freq[author] = 1
 	
+	#sort this mapping so that we retrieve the most frequent author(s) 
 	max_authors = [keys for keys, values in author_freq.items() if values == max(author_freq.values())]
 
 	author_book_set = set()
 	books_list = []
+
+	# retrieve all books from most frequent author(s)
+	# there maybe multiple authors sharing the max frequency
 	for author_name in max_authors:
 		author = Author.query.filter_by(name = author_name).first()
 		books = Book_author.query.filter_by(author_id = author.author_id).all()
@@ -110,7 +145,17 @@ def favouriteAuthor():
 	books_list.sort(key = lambda x: x['rating'], reverse=True)
 	return dumps({'favourite_authors' : max_authors, 'books' : books_list})
 
+"""
+Description: 
+- Get recommendation based the users the current user follows
 
+Args:
+- token String: to be used for session validation
+
+Return:
+- dict: Dictionary containing list of Favourite books from followings of user
+
+"""	
 @app.route(url_prefix + "/favouriteFollowed", methods=["GET"])
 def favouriteFollowed():
 	token = request.args.get('token')
@@ -119,6 +164,8 @@ def favouriteFollowed():
 	followed_users = Follow_relationship.query.filter_by(follower_user_id = user.user_id).all()
 	book_set = set()
 	books_list = []
+
+	#get all books from all collections from all users the current user follows
 	for user in followed_users:
 		collections = Collection.query.filter_by(user_id = user.followed_user.user_id)
 		for collection in collections:
@@ -134,10 +181,11 @@ def favouriteFollowed():
 				    'average_rating' : book.average_rating,
 				    'publish_date': book.publish_date
                 }
+				#the set is maintained only for O(1) checkup of the book dict
 				if book.book_id not in book_set:
 					books_list.append(book_dict)
 					book_set.add(book.book_id)
 
-
+	
 	books_list.sort(key = lambda x: x['average_rating'], reverse=True)
 	return dumps({'favourite_followed_books' : books_list})
